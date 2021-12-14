17fbfeb83c6ef85e587e3c4f59139bb907fd0d01
Donato Manzione <donato.manz.sc@gmail.com>
2021-12-14 13:00:08 -0500

    added swap method to bubble sort class

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -3,19 +3,29 @@ package sorting;
 import utilities.ArrayUtility;
 
 public class BubbleSort {
+	private static int[] inputArray;
 
-	public static int[] sort(int[] inputArray) {
+	public static int[] sort(int[] arr) {
+		inputArray = arr;
+		//iterate array from index 0 to next to last index
 		for (int i = 0; i < inputArray.length - 1; i++) {
-			if (inputArray[i] > inputArray[i + 1]) {
-				int temp = inputArray[i];
-				inputArray[i] = inputArray[i + 1];
-				inputArray[i + 1] = temp;
+			int elementAtIndex = inputArray[i];
+			int nextElement = inputArray[i+1];
+			//if element at index is greater than next element, swap
+			if (elementAtIndex > nextElement) {
 
+				swap(i, i + 1);
 			}
 		}
-		
+
 		ArrayUtility.print(inputArray);
 		return inputArray;
+	}
+
+	public static void swap(int firstIndex, int secondIndex) {
+		int temp = inputArray[firstIndex];
+		inputArray[firstIndex] = inputArray[secondIndex];
+		inputArray[secondIndex] = temp;
 	}
 
 }
